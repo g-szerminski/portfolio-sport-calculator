45000e68738bbcc25d25db52919c613c7d65bd0f
Grzegorz Szermiński <szerman@poczta.fm>
2019-09-15 20:58:41 +0200

    js code restrucrurasation - work in progress

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-const aboutBmiButton = document.querySelector(".aboutBmiButton");
-const checkBmiButton = document.querySelector(".checkBmiButton");
+const btn = document.querySelector(".btn");
 
-const aboutBmi = () => {
-  if (checkBmiButton.classList.contains("active")) {
-    checkBmiButton.classList.toggle("active");
+const changeClassActive = () => {
+  if (btn.classList.contains("active")) {
+    btn.classList.toggle("active");
   }
-  aboutBmiButton.classList.toggle("active");
-};
-const checkYourBmi = () => {
-  if (aboutBmiButton.classList.contains("active")) {
-    aboutBmiButton.classList.toggle("active");
-  }
-  checkBmiButton.classList.toggle("active");
+  btn.classList.toggle("active");
 };
 
-aboutBmiButton.addEventListener("click", aboutBmi);
-checkBmiButton.addEventListener("click", checkYourBmi);
+btn.addEventListener("click", changeClassActive);
+
+// const aboutBmiButton = document.querySelector(".aboutBmiButton");
+// const checkBmiButton = document.querySelector(".checkBmiButton");
+// const aboutBmi = () => {
+//   if (checkBmiButton.classList.contains("active")) {
+//     checkBmiButton.classList.toggle("active");
+//   }
+//   aboutBmiButton.classList.toggle("active");
+// };
+// const checkYourBmi = () => {
+//   if (aboutBmiButton.classList.contains("active")) {
+//     aboutBmiButton.classList.toggle("active");
+//   }
+//   checkBmiButton.classList.toggle("active");
+// };
+// aboutBmiButton.addEventListener("click", changeClassActive);
+// checkBmiButton.addEventListener("click", changeClassActive);
