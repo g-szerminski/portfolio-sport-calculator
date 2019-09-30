4676d90aee4bd02c502b3222bbb409cc78201903
Grzegorz Szermiński <szerman@poczta.fm>
2019-09-30 20:41:15 +0200

    work in progress

--- script.js
+++ script.js
@@ -13,21 +13,21 @@ btn.forEach(item => {
         const removeActive = () => item.classList.remove("active");
         removeActive();
       });
+
       item.classList.add("active");
-      // activeCalc.style.display = "block";
-      // activeCalc.setAttribute("target", "_blank");
-      activeCalc.style.width = 70 + "%";
-      activeCalc.style.height = 50 + "%";
-      h2.textContent = item.textContent;
+      activeCalc.style.width = 55 + "%";
+      activeCalc.style.height = 40 + "%";
+      setTimeout(() => (h2.textContent = item.textContent), 200);
       activeCalc.addEventListener("click", function() {
         activeCalc.style.width = 0;
         activeCalc.style.height = 0;
         h2.textContent = "";
         item.classList.remove("active");
+        // activeCalc.style.display = "block";
+        // activeCalc.setAttribute("target", "_blank");
       });
     }
   };
-
   item.addEventListener("click", changeClassActive);
 });
 
