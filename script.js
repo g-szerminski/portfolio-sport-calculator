3154680ac33f7e84c01f685b9fb71e4abc459c90
Grzegorz Szermiński <szerman@poczta.fm>
2019-10-06 12:35:37 +0200

    work in progress

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const btn = document.querySelectorAll(".btn");
-const button = document.querySelector("button");
 const activeCalc = document.querySelector(".activeCalc");
 const h2 = document.querySelector(".h2");
 const visuallyhidden = document.querySelector(".visuallyhidden");
 const close = document.querySelector(".close");
-
+let content = document.querySelector(".content");
+// const button = document.querySelector("button");
 // const arr = Array.from(document.querySelectorAll(".btn"));
 
 btn.forEach(item => {
@@ -21,16 +21,16 @@ btn.forEach(item => {
       activeCalc.style.width = 55 + "%";
       activeCalc.style.height = 40 + "%";
       close.style.visibility = "visible";
-
-      // if (item.classList.contains("active"))
       visuallyhidden.classList.add("popup-overlay");
-
+      content.style.visibility = "visible";
       setTimeout(() => (h2.textContent = item.textContent), 200);
-      activeCalc.addEventListener("click", function() {
+      // if (item.classList.contains("active"))
+      close.addEventListener("click", function() {
         activeCalc.style.width = 0;
         activeCalc.style.height = 0;
         close.style.visibility = "hidden";
         h2.textContent = "";
+        content.style.visibility = "hidden";
         item.classList.remove("active");
         visuallyhidden.classList.remove("popup-overlay");
       });
@@ -39,15 +39,25 @@ btn.forEach(item => {
   item.addEventListener("click", changeClassActive);
 });
 
-// class Calculator {
-//   constructor(name) {
-//     this.name = name;
-//   }
-//   showAbout() {
-//     if (button.classList.contains("active")) {
-//       activeCalc.style.display = "block";
-//     }
-//   }
-// }
-// const calc = new Calculator("bmi");
-// calc.showAbout();
+class Calculator {
+  constructor(name, about, ...data) {
+    this.name = name;
+    this.about = about;
+    this.data = data;
+  }
+  showAbout(text) {
+    console.log("pokazano about");
+    content.textContent = this.about;
+    // this.about = content.textContent;
+    // this.textContent = content;
+    // this.content = content;
+    // return content;
+    // this.about.textContent = text;
+  }
+  pushData(addData) {
+    this.data.push(addData);
+  }
+}
+
+const bmi = new Calculator("bmi", "to jest ten content", "0000");
+bmi.showAbout();
