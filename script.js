b2a7a314fbb471432d6ba7656b7a1e9ffb7fa44d
Grzegorz Szermiński <szerman@poczta.fm>
2019-10-07 12:04:08 +0200

    change classes names and add popupClose function

--- script.js
+++ script.js
@@ -1,63 +1,58 @@
 const btn = document.querySelectorAll(".btn");
 const activeCalc = document.querySelector(".activeCalc");
-const h2 = document.querySelector(".h2");
-const visuallyhidden = document.querySelector(".visuallyhidden");
+const popupHeader = document.querySelector(".popupHeader");
+const darkBackground = document.querySelector(".darkBackground");
 const close = document.querySelector(".close");
-let content = document.querySelector(".content");
-// const button = document.querySelector("button");
-// const arr = Array.from(document.querySelectorAll(".btn"));
+let popupContent = document.querySelector(".popupContent");
 
 btn.forEach(item => {
   const changeClassActive = () => {
     if (item.classList.contains("active")) {
       item.classList.remove("active");
-    } else {
-      btn.forEach(item => {
-        const removeActive = () => item.classList.remove("active");
-        removeActive();
-      });
-
-      item.classList.add("active");
-      activeCalc.style.width = 55 + "%";
-      activeCalc.style.height = 40 + "%";
-      close.style.visibility = "visible";
-      visuallyhidden.classList.add("popup-overlay");
-      content.style.visibility = "visible";
-      setTimeout(() => (h2.textContent = item.textContent), 200);
-      // if (item.classList.contains("active"))
-      close.addEventListener("click", function() {
-        activeCalc.style.width = 0;
-        activeCalc.style.height = 0;
-        close.style.visibility = "hidden";
-        h2.textContent = "";
-        content.style.visibility = "hidden";
-        item.classList.remove("active");
-        visuallyhidden.classList.remove("popup-overlay");
-      });
     }
+    const popupClose = () => {
+      activeCalc.style.width = 0;
+      activeCalc.style.height = 0;
+      close.style.visibility = "hidden";
+      popupContent.style.visibility = "hidden";
+      popupHeader.textContent = "";
+      item.classList.remove("active");
+      darkBackground.classList.remove("popup-overlay");
+    };
+
+    item.classList.add("active");
+    activeCalc.style.width = 40 + "%";
+    activeCalc.style.height = 40 + "%";
+    close.style.visibility = "visible";
+    darkBackground.classList.add("popup-overlay");
+    popupContent.style.visibility = "visible";
+    setTimeout(() => (popupHeader.textContent = item.textContent), 200);
+    close.addEventListener("click", popupClose);
+    darkBackground.addEventListener("click", popupClose);
   };
+
   item.addEventListener("click", changeClassActive);
 });
 
-class Calculator {
-  constructor(name, about, ...data) {
-    this.name = name;
-    this.about = about;
-    this.data = data;
-  }
-  showAbout(text) {
-    console.log("pokazano about");
-    content.textContent = this.about;
-    // this.about = content.textContent;
-    // this.textContent = content;
-    // this.content = content;
-    // return content;
-    // this.about.textContent = text;
-  }
-  pushData(addData) {
-    this.data.push(addData);
-  }
-}
+// class Calculator {
+//   constructor(name, about, ...data) {
+//     this.name = name;
+//     this.about = about;
+//     this.data = data;
+//   }
+//   showAbout(text) {
+//     console.log("pokazano about");
+//     content.textContent = this.about;
+//     // this.about = content.textContent;
+//     // this.textContent = content;
+//     // this.content = content;
+//     // return content;
+//     // this.about.textContent = text;
+//   }
+//   pushData(addData) {
+//     this.data.push(addData);
+//   }
+// }
 
-const bmi = new Calculator("bmi", "to jest ten content", "0000");
-bmi.showAbout();
+// // const bmi = new Calculator("bmi", "to jest ten content", "0000");
+// // bmi.showAbout();
