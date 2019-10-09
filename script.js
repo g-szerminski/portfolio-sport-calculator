0d11dcd52f5c7f28e09f097491c87b239a4c744d
Grzegorz Szermiński <szerman@poczta.fm>
2019-10-09 12:00:55 +0200

    add display and show classes in css and change display functions in js file

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
 const btn = document.querySelectorAll(".btn");
-const activeCalc = document.querySelector(".activeCalc");
+const popup = document.querySelector(".popup");
 const popupHeader = document.querySelector(".popupHeader");
 const darkBackground = document.querySelector(".darkBackground");
-const close = document.querySelector(".close");
+const popupClose = document.querySelector(".popupClose");
 const popupContent = document.querySelector(".popupContent");
+let calcDescription = "zzzzzzzzzzzz";
 
 btn.forEach(item => {
   const popupDisplay = () => {
-    activeCalc.style.width = 40 + "%";
-    activeCalc.style.height = 40 + "%";
-    close.style.visibility = "visible";
-    popupContent.style.visibility = "visible";
-    // item.classList.add("active");
+    popup.classList.add("popupDisplay");
+    popupHeader.classList.add("show");
+    popupContent.classList.add("show");
+    popupClose.classList.add("show");
     darkBackground.classList.add("popup-overlay");
-    setTimeout(() => (popupHeader.textContent = item.textContent), 1000);
+    item.classList.add("active");
+    setTimeout(() => {
+      popupHeader.textContent = item.textContent;
+      popupContent.textContent = calcDescription;
+    }, 1000);
   };
-  const popupClose = () => {
-    activeCalc.style.width = 0;
-    activeCalc.style.height = 0;
-    close.style.visibility = "hidden";
-    popupContent.style.visibility = "hidden";
+  const popupHide = () => {
+    popup.classList.remove("popupDisplay");
+    popupHeader.classList.remove("show");
+    popupContent.classList.remove("show");
+    popupClose.classList.remove("show");
     popupHeader.textContent = "";
+    popupContent.textContent = "";
     item.classList.remove("active");
     darkBackground.classList.remove("popup-overlay");
   };
-  const changeBtnActive = () => {
-    if (item.classList.contains("active")) {
-      item.classList.remove("active");
-    }
-    item.classList.add("active");
-  };
 
-  item.addEventListener("click", changeBtnActive);
   item.addEventListener("click", popupDisplay);
-  close.addEventListener("click", popupClose);
-  darkBackground.addEventListener("click", popupClose);
+  popupClose.addEventListener("click", popupHide);
+  darkBackground.addEventListener("click", popupHide);
 });
 
 // class Calculator {
@@ -59,3 +57,41 @@ btn.forEach(item => {
 
 // // const bmi = new Calculator("bmi", "to jest ten content", "0000");
 // // bmi.showAbout();
+
+//----------------------------------------------------------------------------------------------------------------
+// const changeBtnActive = () => {
+//   if (item.classList.contains("active")) {
+//     item.classList.remove("active");
+//   }
+//   item.classList.add("active");
+// };
+// item.addEventListener("click", changeBtnActive);
+//---------------------------------------------------
+// btn.forEach(item => {
+//   const popupDisplay = () => {
+//     // popup.style.width = 40 + "%";
+//     // popup.style.height = 40 + "%";
+//     popup.classList.add("popupDisplay");
+//     popupHeader.classList.add("show");
+//     popupContent.classList.add("show");
+//     popupClose.classList.add("show");
+//     // popupClose.style.visibility = "visible";
+//     // popupContent.style.visibility = "visible";
+//     darkBackground.classList.add("popup-overlay");
+//     item.classList.add("active");
+//     setTimeout(() => (popupHeader.textContent = item.textContent), 200);
+//   };
+//   const popupHide = () => {
+//     // popup.style.width = 0;
+//     // popup.style.height = 0;
+//     popup.classList.remove("popupDisplay");
+//     popupHeader.classList.remove("show");
+//     popupContent.classList.remove("show");
+//     popupClose.classList.remove("show");
+//     // popupClose.style.visibility = "hidden";
+//     // popupContent.style.visibility = "hidden";
+//     // popupHeader.textContent = "";
+//     item.classList.remove("active");
+//     darkBackground.classList.remove("popup-overlay");
+//   };
+//---------------------------------------------
